b7c9e1a5c11407229a9895a85be8be41926cfa69
chin13577 <chinnawat13577@gmail.com>
2015-12-15 16:05:44 +0700

    group5_consiment_web

--- dump_database_consiment_web/DROP.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE "SYSTEM"."CON_USER" cascade constraints;
-DROP TABLE "SYSTEM"."CON_TRANSACTION" cascade constraints;
-DROP TABLE "SYSTEM"."CON_PRODUCT" cascade constraints;
-DROP TABLE "SYSTEM"."CON_PHOTO" cascade constraints;
